da5123f3cca2149ff7983a1e550527083a5976f2
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-13 00:03:14 -0300

    array interno

--- array-slices/main.go
+++ array-slices/main.go
@@ -40,4 +40,49 @@ func main() {
 
 	array2[1] = "Posição Alterada"
 	fmt.Println(slice2) //[Posição Alterada posição 3]
+
+	//ARRAYS INTERNOS
+	fmt.Println("-------------------------")
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)      // [0 0 0 0 0 0 0 0 0 0]
+	fmt.Println(len(slice3)) //lenght  //10
+	fmt.Println(cap(slice3)) //capacidadee //11
+
+	slice3 = append(slice3, 11) // aqui atingiu o limite to slice que aponta p array
+	slice3 = append(slice3, 12) // aqui eu add mais um, estourando !!!
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // 12 *****-
+	fmt.Println(cap(slice3)) // 24 *****-  go além de deixar incluir, ainda dobrou a capacidade de alocação, por conta disso que não declaro o tamanho no slice, pois é dinamico sacou, só nos arraysss
+
+	//O TAMANHO É OPCIONALIS
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 6) // adiconando é aquilo , tam 6 e capacidade de 12
+	fmt.Println(len(slice4))   //5
+	fmt.Println(cap(slice4))   //5
+	//LOGO, A CAPACIDADE VAI SER O PRÓPRIO TAMANHO DELE
+
+	//SLICE É UMA LISTA SEM TAM FIXO
+	//ARRAY É UMA LISTA DE TAM FIXO
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	slice7 := make([]int64, 10, 11)
+	slice7 = append(slice7, 11)
+	slice7 = append(slice7, 12)
+	fmt.Println("slice7", slice7)
+	fmt.Println(len(slice7))
+	fmt.Println(cap(slice7))
+
 }
